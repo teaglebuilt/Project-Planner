eeed3b6c42f3d24fe6f2b14de0bdbfcbf2287a2a
teaglebuilt <dillan.teagle.va@gmail.com>
2018-09-26 16:15:40 -0500

    init

---
+++ project_planner/src/components/projects/ProjectSummary.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const ProjectSummary = () => {
+    return(
+        <div className="project-list section">
+          <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text text-darken-3">
+              <span className="card-title">Project Title</span>
+              <p>Posted By</p>
+              <p className="grey-text">Time</p>
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default ProjectSummary;
